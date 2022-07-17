0df799bb025129e7396d4121a2b8970f25deb8e9
Martin Bonnin <martin@mbonnin.net>
2022-07-17 19:08:21 +0200

    add a version

--- src/commonMain/kotlin/pga/MainCommand.kt
+++ src/commonMain/kotlin/pga/MainCommand.kt
@@ -2,13 +2,24 @@ package pga
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 
-class MainCommand: CliktCommand() {
+class MainCommand: CliktCommand(
+  name = "pin-github-actions",
+  invokeWithoutSubcommand = true
+) {
   init {
     subcommands(PinCommand(), UpdateCommand(), LoginCommand(), LogoutCommand())
   }
 
-  override fun run() {
+  private val version by option().flag()
 
+  override fun run() {
+    if (version) {
+      echo("pin-github-actions $VERSION")
+    } else {
+      echo(getFormattedHelp())
+    }
   }
 }
---
+++ src/commonMain/kotlin/pga/version.kt
@@ -0,0 +1,3 @@
+package pga
+
+const val VERSION = "0.1.0"
